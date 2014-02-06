35646715991306c7b85c76fea02626849e292154
mohamedniyaz <zackriyaniyas@gmail.com>
2014-02-06 05:02:40 -0500

    Push 2

--- BrightBytes/src/StringCalcTest.java
+++ BrightBytes/src/StringCalcTest.java
@@ -31,5 +31,26 @@ public class StringCalcTest {
         int sumOfNumbers = 5 + 13;
         assertEquals(StringCalc.calculate(pairOfNumbers), sumOfNumbers);
     }
+
+    @Test
+    public void givenMoreThanTwoComaSeparatedNumbersReturnsTheirSum() throws Exception {
+        String manyNumbers = "1,2,3,4";
+        int sumOfManyNumbers = 1 + 2 + 3 + 4;
+        assertEquals(StringCalc.calculate(manyNumbers), sumOfManyNumbers);
+    }
+    
+    @Test
+    public void allowsUsingNewLinesAsSeparators() throws Exception {
+        String numbersSeparatedWithNewLines = "3\n4\n6";
+        int sumOfNumbers = 3 + 4 + 6;
+        assertEquals(StringCalc.calculate(numbersSeparatedWithNewLines), sumOfNumbers);
+    }
+    
+    @Test
+    public void allowsMixingComasOrNewLinesAsSeparators() throws Exception {
+        String numbersSeparatedWithNewLinesAndCommas = "2,3\n4";
+        int sumOfNumbers = 2 + 3 + 4;
+        assertEquals(StringCalc.calculate(numbersSeparatedWithNewLinesAndCommas), sumOfNumbers);
+    }
     
 }
